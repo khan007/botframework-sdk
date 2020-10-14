5757877973a58b342675b4831e8c4cc4a2b96d39
virtual-josh <virtual-josh@server.no>
2020-10-13 17:31:21 -0700

    adding dotnet bot

---
+++ testbots/dotnet/singleturn/WeatherSkillBot/Weather.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples.EchoSkillBot
+{
+    [JsonArray]
+    public class WeatherReport
+    {
+        [JsonProperty("properties")]
+        public Newtonsoft.Json.Linq.JObject report;
+    }
+
+    public class SingleDay
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("temperature")]
+        public string Temperature { get; set; }
+
+        [JsonProperty("shortForecast")]
+        public string ShortForecast { get; set; }
+    }
+}
